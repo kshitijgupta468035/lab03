424ccbbc1454b6bde353c848598849c0dc29a35c
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2019-08-20 00:06:23 +0530

    add conditions with output statement

--- src/question6/Question6.java
+++ src/question6/Question6.java
@@ -4,14 +4,17 @@ import java.util.Scanner;
 
 public class Question6 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner();
-        char char1 = scanner.nextLine().trim().charAt(0);
-        char char2 = scanner.nextLine().trim().charAt(0);
+        Scanner scanner = new Scanner(System.in);
+        char char1 = scanner.next().trim().charAt(0);
+        char char2 = scanner.next().trim().charAt(0);
+        if (char1 > char2) {
+            System.out.println((char) (char1) + " " + (char) (char2));
+        } else {
+            System.out.println((char) (char2) + " " + (char) (char1));
+        }
 
 
         scanner.close();
-
-
 
 
     }
