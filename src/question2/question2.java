57d687486eb342d0fef28c1d8f893e41acc96ea0
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2019-08-19 14:49:08 +0530

    add input for variable 1:(value1, String) add input for variable 2:(value2, String)

--- src/question2/question2.java
+++ src/question2/question2.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class question2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String value1 = scanner.next().trim();
+        String value2 = scanner.next().trim();
+
+
 
 
 
