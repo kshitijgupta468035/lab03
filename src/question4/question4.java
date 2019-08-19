526c01d984d88f528a31d2b00f9132a28930f51b
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2019-08-19 15:32:29 +0530

    add three conditions with their output statement

--- src/question4/question4.java
+++ src/question4/question4.java
@@ -6,6 +6,13 @@ public class question4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number == 0) {
+            System.out.println("Zero");
+        } else {
+            System.out.println("Negative");
+        }
 
         scanner.close();
 
