c7e052ae23c83db882a97694d2e6838c7b7a9bc2
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2019-08-19 14:56:22 +0530

    add object scanner for class Scanner

--- src/question3/question3.java
+++ src/question3/question3.java
@@ -1,7 +1,12 @@
 package question3;
 
+import java.util.Scanner;
+
 public class question3 {
     public static void main(String[] args) {
+        Scanner scanner = new sacnner(System.in);
+
+
 
     }
 
