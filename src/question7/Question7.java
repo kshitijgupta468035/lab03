80f917afbbfeffcaba539bd2b0e61034524e36cb
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2019-08-20 00:22:11 +0530

    add input variable:(value, char)

--- src/question7/Question7.java
+++ src/question7/Question7.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Question7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        char value = scanner.next().trim().charAt(0);
         scanner.close();
     }
 }
