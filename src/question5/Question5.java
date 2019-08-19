502e767089be035a3f10366f7bbb394d92545dfc
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2019-08-19 23:42:01 +0530

    add input varaible:(number, Int)

--- src/question5/Question5.java
+++ src/question5/Question5.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Question5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
 
         scanner.close();
 
