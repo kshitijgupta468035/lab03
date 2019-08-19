a4320cf8613ef97b83e548aef0ca300c09ef0ec2
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2019-08-19 23:51:39 +0530

    add two input variable:(char1, Char),(char2, Char)

--- src/question6/Question6.java
+++ src/question6/Question6.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class Question6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner();
+        char char1 = scanner.nextLine().trim().charAt(0);
+        char char2 = scanner.nextLine().trim().charAt(0);
+
 
         scanner.close();
 
